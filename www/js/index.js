f65bdf121191aea8a0c6da1c82664e4d53e47774
esraltintas <es.altintas@gmail.com>
2014-12-14 17:31:16 +0200

    Fixed font-size problem related #8

--- www/js/index.js
+++ www/js/index.js
@@ -49,17 +49,11 @@ var app = {
 };
 
 var apiRoot = 'http://api.ozdincer.com';
+var projects = [];
 function startApp() {
     request(apiRoot + '/api/v1/languages/', function(data){
-        if (JSON.parse(window.localStorage.getItem('db')))
-            allData = JSON.parse(window.localStorage.getItem('db'));
-        else
-            createData(data);
-
-        window.location.hash = '#languagesPage';
-        console.log(data)
         projects = data.objects;
-
+        $.mobile.changePage( "#languagesPage");
     }, function() {
         navigator.notification.alert(
             'Kullanici adi veya parola hatali',  // message
@@ -71,99 +65,139 @@ function startApp() {
     var selectedlanguage = '';
     var selectedlanguageIndex = 0;
     var selectedProjectIndex = 0;
+    var selectedStoreIndex = 0;
 
-    $(document).on("pageshow","#languagesPage",function() {
-        var markup = '<ul data-role="listview" data-theme="b">';
+    $(document).on("pageshow", "#languagesPage",function() {
+        var markup = '';
         for (var i = 0; i < projects.length; i++) {
-            markup += '<li> <a class="language" data-code="'+ i +'"'+' >' + (i + 1) + ' - ' + projects[i].fullname + '</a> </li>';
+            markup += '<li class="language" data-code="'+ i +'"> ' + projects[i].fullname + ' </li>';
         }
 
-        markup += '</ul>';
         $('#Languages_Content').html(markup);
-        $('a.language').on("tap", function() {
+        $("#Languages_Content").listview( "refresh" );
+
+        $('.language').on("tap", function() {
             selectedlanguageIndex = $(this).attr("data-code");
             console.log(selectedlanguageIndex)
-            $.mobile.changePage( "#projectsPage");
+            getSelectedLanguageData(selectedlanguageIndex, function() {
+                $.mobile.changePage( "#projectsPage");
+            });
         });
     });
 
 
     $(document).on("pageshow","#projectsPage",function() {
-        var language = allData[selectedlanguageIndex];
-        var markup = '<ul data-role="listview" data-theme="b">';
+        var language = projects[selectedlanguageIndex];
+        console.log(language)
+
+        var markup = '';
         for (var i = 0; i < language.translation_projects.length; i++) {
-            real_path = language.translation_projects[i].real_path.split("/")[0];
-            markup += '<li> <a class="project" data-code="' + i +'"'+' >' + real_path + '</a> </li>';
+            var real_path = language.translation_projects[i].real_path.split("/")[0];
+            markup += '<li class="project" data-code="' + i +'"> ' + real_path + ' </li>';
         }
-        markup += '</ul>';
 
         $('#Projects_Content').html(markup);
+        $("#Projects_Content").listview( "refresh" );
 
-        $('a.project').on("tap", function() {
+        $('.project').on("tap", function() {
             selectedProjectIndex = $(this).attr("data-code");
-            $.mobile.changePage( "#filesPage");
+            getSelectedProjectData(selectedlanguageIndex, selectedProjectIndex, function() {
+                $.mobile.changePage( "#filesPage");
+            });
         });
+
+    $(document).on("pageshow","#filesPage",function() {
+        var stores = projects[selectedlanguageIndex].translation_projects[selectedProjectIndex].stores;
+        console.log(stores)
+        var markup = '';
+        for (var i = 0; i < stores.length; i++) {
+            var progress = stores[i]['statistics']['translated']['percentage'];
+            markup += '<tr><td><a class="store" data-code="' + i +'">' + stores[i].name + '</a></td>' +
+                '<td><progress value="'+ progress +'" max="100"></progress></td></tr>';
+        }
+
+        $('#Files_Content').html(markup);
+        $("#fileTable").table( "refresh" );
+
+
+        $('.store').on("tap", function() {
+            selectedStoreIndex = $(this).attr("data-code");
+            $.mobile.changePage( "#translatePage");
+        });
+    });
 
     });
 
     $(document).on("pageshow","#filesPage",function() {
-        var stores = allData[selectedlanguageIndex].translation_projects[selectedProjectIndex].stores;
-        var markup = '<ul data-role="listview" data-theme="b">';
+        var stores = projects[selectedlanguageIndex].translation_projects[selectedProjectIndex].stores;
+        console.log(stores)
+        var markup = '';
         for (var i = 0; i < stores.length; i++) {
-            markup += '<li> <a href="#"' + ' >' + stores[i].name + '</a> </li>';
+            var progress = stores[i]['statistics']['translated']['percentage'];
+            markup += '<tr><td><a class="store" data-code="' + i +'">' + stores[i].name + '</a></td>' +
+                '<td><progress value="'+ progress +'" max="100"></progress></td></tr>';
         }
-        markup += '</ul>';
+
         $('#Files_Content').html(markup);
+        $("#fileTable").table( "refresh" );
+
+
+        $('.store').on("tap", function() {
+            selectedStoreIndex = $(this).attr("data-code");
+            $.mobile.changePage( "#translatePage");
     });
+
+});
 
 
 }
 
-var allData = [];
-var translationProjectCount = 0;
-var storesCount = 0;
-function createData(data) {
-    allData = data.objects;
+function getSelectedLanguageData(languageIndex, callback) {
+    var language = projects[languageIndex];
+    var translationProjectCount = language.translation_projects.length;
+    var completedRequestCount = 0;
 
-    for (var i = 0; i < allData.length; i++) {
-        var translationProjects = allData[i]['translation_projects'];
-        for (var j = 0; j < translationProjects.length; j++) {
-            updateTranslationProject(i, j, apiRoot + translationProjects[j]);
+    for (var i = 0; i < language.translation_projects.length; i++) {
+        updateTranslationProject(languageIndex, i, apiRoot + language.translation_projects[i], isCompleted)
+    }
+
+    function isCompleted() {
+        completedRequestCount++;
+        if (completedRequestCount == translationProjectCount) {
+            callback();
         }
     }
 }
 
-function updateTranslationProject(i, j, url) {
+function updateTranslationProject(i, j, url, callback) {
     request(url, function(data) {
-        allData[i]['translation_projects'][j] = data;
-        translationProjectCount++;
-        console.log(translationProjectCount)
-        if (translationProjectCount == 65) {
-            startStores();
-        }
+        projects[i]['translation_projects'][j] = data;
+        callback()
     });
 }
 
-function startStores() {
-    for (var i = 0; i < allData.length; i++) {
-        var translationProjects = allData[i]['translation_projects'];
-        for (var j = 0; j < translationProjects.length; j++) {
-            var stores = translationProjects[j]['stores'];
-            for (var k = 0; k < stores.length; k++) {
-                updateStores(i, j, k, apiRoot + stores[k]);
-            }
+function getSelectedProjectData(languageIndex, projectIndex, callback) {
+    var stores = projects[languageIndex]['translation_projects'][projectIndex]['stores'];
+    var storesCount = stores.length;
+    var completedRequestCount = 0;
+
+    for (var i = 0; i < stores.length; i++) {
+        updateStores(languageIndex, projectIndex, i, apiRoot + stores[i] + 'statistics/', isCompleted)
+    }
+
+    function isCompleted() {
+        completedRequestCount++;
+        if (completedRequestCount == storesCount) {
+            callback();
         }
     }
 }
 
-function updateStores(i, j, k, url) {
+
+function updateStores(i, j, k, url, callback) {
     request(url, function(data) {
-        allData[i]['translation_projects'][j]['stores'][k] = data;
-        storesCount++;
-        console.log(storesCount)
-        if (storesCount == 74) {
-            window.localStorage.setItem('db', JSON.stringify(allData));
-        }
+        projects[i]['translation_projects'][j]['stores'][k] = data;
+        callback();
     });
 };
 
@@ -173,7 +207,7 @@ function request(url, successCallback, errorCallback) {
     $.ajax({
         type: "GET",
         url: url,
-        dataType: 'jsonp',
+        dataType: 'json',
         async: false,
         data: '{}',
         beforeSend: function (xhr) {
@@ -189,4 +223,3 @@ function request(url, successCallback, errorCallback) {
         return "Basic " + hash;
     };
 }
- 
